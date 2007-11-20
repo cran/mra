0731fe5047d3eb0c3c4a45868a6012cceba430d1
Trent McDonald <tmcdonald@west-inc.com>
2007-11-20 00:00:00 +0000

    version 1.1

--- R/zzz.R
+++ R/zzz.R
@@ -2,5 +2,5 @@
 
 	library.dynam("mra", pkgname)
 
-	cat(paste( "This is (M)ark (R)ecapture (A)nalysis.", "\n\nTrent McDonald, WEST Inc.\n(tmcdonald@west-inc.com, www.west-inc.com)\n") )
+	cat(paste( "Mark-Recapture Analysis.", "\n\nTrent McDonald, WEST Inc.\n(tmcdonald@west-inc.com, www.west-inc.com)\n") )
 }
